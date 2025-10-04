5c4c9626da6ec12dc8a09b5b8a05c995f4cb5ac1
Jazarelly Zapata Minaya <jazarelly_zapata@usmp.pe>
2025-10-04 13:38:56 -0500

    creacion de archivos

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PARCIAL_programaci_n.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
