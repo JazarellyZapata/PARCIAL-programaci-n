ada3ac20b673334f191044ef73a36be05998ccaf
Jazarelly Zapata Minaya <jazarelly_zapata@usmp.pe>
2025-10-06 22:17:45 -0500

    correccion

--- obj/Debug/net9.0/PARCIAL-programaci-n.AssemblyInfo.cs
+++ obj/Debug/net9.0/PARCIAL-programaci-n.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PARCIAL-programaci-n")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c691dcf0f96fd7ad08c969b2758c936a41e57af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66a0a43cccaf62f802ad9688bc691083331759f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PARCIAL-programaci-n")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PARCIAL-programaci-n")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
